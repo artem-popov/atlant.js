e159a813af8753a8f57e226bec24a06908272f6d
Artem Popov <artem.popov@frumatic.com>
2014-08-15 13:25:47 +0600

    cleanup, rename fakePushState to appropriate name

---
+++ src/inc/wrapPushState.js
@@ -0,0 +1,31 @@
+
+/**
+ * Create fake push state
+ * Use like that:
+ * require('fakePushState')(window);
+ * It will patch window.history to rise "pushstate" event when pushstate is happend.
+ **/
+var wrapPushState = function(window){
+    var pushState = window.history.pushState;
+
+    var tryState = function(params) {
+        try { 
+           return pushState.apply(window.history, params); 
+        } catch (e) {
+           console.log('Can\'t push state:', e);
+           return void 0;
+        }
+    };
+
+    window.history.pushState = function(state, title, url) {
+        var newState;
+        var onpushstate = new CustomEvent('pushstate', { detail: { state: state, title: title, url: url } } );
+        window.dispatchEvent(onpushstate);
+
+        return tryState(arguments);
+    };
+
+};
+
+
+module.exports = wrapPushState;
