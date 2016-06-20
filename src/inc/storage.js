f241000362c46c6e06b426d70d42698ea3d5f15e
Artem Popov <artem.popov@outlook.com>
2016-06-20 13:05:33 +0500

    eslint

--- src/inc/storage.js
+++ src/inc/storage.js
@@ -1,33 +1,33 @@
-var Storage = {
-    storage: typeof window !== 'undefined' && window.localStorage,
-    listen: function(){
-        window.addEventListener('storage', this.onChange)
-    },
-    stopListen: function(){
-        window.removeEventListener('storage', this.onChange)
-    },
-    onChange: function(key, newValue, oldValue, storageArea, url){
-        console.log('storage changed',key, newValue, oldValue, storageArea, url)
-    },
-    setStorage: function(storage){
-        this.storage = storage
-    },
-    persist: function(storeName, newState){
-        if (!this.storage) return void 0;
+const Storage = {
+  storage: typeof window !== 'undefined' && window.localStorage,
+  listen() {
+    window.addEventListener('storage', this.onChange);
+  },
+  stopListen() {
+    window.removeEventListener('storage', this.onChange);
+  },
+  onChange(key, newValue, oldValue, storageArea, url) {
+    console.log('storage changed', key, newValue, oldValue, storageArea, url);
+  },
+  setStorage(storage) {
+    this.storage = storage;
+  },
+  persist(storeName, newState) {
+    if (!this.storage) return void 0;
 
-        // console.time('persist'+ storeName)
-        this.storage.setItem(storeName, JSON.stringify(newState) );
-        // console.timeEnd('persist'+ storeName)
-        return
-    },
-    load: function(storeName){
-        if (!this.storage) return void 0;
+    // console.time('persist'+ storeName)
+    this.storage.setItem(storeName, JSON.stringify(newState));
+    // console.timeEnd('persist'+ storeName)
+    return void 0;
+  },
+  load(storeName) {
+    if (!this.storage) return void 0;
 
-        // console.time('load'+ storeName)
-        var value = JSON.parse(this.storage.getItem(storeName));
-        // console.timeEnd('load'+ storeName)
-        // console.log(storeName, 'value:', value)
-        return value
-    }
-}
+    // console.time('load'+ storeName)
+    const value = JSON.parse(this.storage.getItem(storeName));
+    // console.timeEnd('load'+ storeName)
+    // console.log(storeName, 'value:', value)
+    return value;
+  },
+};
 module.exports = Storage;
